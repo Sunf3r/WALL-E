dd4a21e3edafc50b49ef7c45c446aa68fa964c10
Sunf3r <sunf3r@proton.me>
2023-07-02 06:59:42 -0300

    add comments in eval

--- src/Commands/Admin/eval.ts
+++ src/Commands/Admin/eval.ts
@@ -11,21 +11,23 @@ export default class implements Command {
 
 	public run = async (bot: BotClient, msg: Msg, args: string[]) => {
 		const startTime = Date.now(),
-			initialRam = (process.memoryUsage().rss / 1024 / 1024).toFixed(2);
+			initialRam = (process.memoryUsage().rss / 1024 / 1024).toFixed(2); // DENO
 
 		const code = args.join(' ');
 		let evaled, title;
 
 		try {
+			// Roda o eval em uma função assíncrona se ele conter a palavra "await"
 			if (code.includes('await')) evaled = await eval(`(async () => { ${code} })()`);
 			else evaled = await eval(code);
 
-			title = '🎉 Retorno';
-			evaled = !evaled ? '- Sem retorno.' : inspect(evaled);
+			title = '🎉 Retorno'; // Título da msg
+			evaled = !evaled ? '- Sem retorno.' : inspect(evaled); // Retorno do eval
 		} catch (error) {
-			title = '❌ Falha';
-			evaled = error;
+			title = '❌ Falha'; // Título da msg
+			evaled = error; // Retorno do eval
 		} finally {
+			// Consumo de RAM ao final do eval
 			const currentRam = (process.memoryUsage().rss / 1024 / 1024).toFixed(2);
 
 			const text = `⏰ *Duração:* ${Date.now() - startTime}ms\n` +
