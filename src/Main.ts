95a040e0786046253551f27cc2df41a3ec7bb4bd
Sunf3r <sunf3r@proton.me>
2023-07-02 06:58:40 -0300

    small aesthetic changes

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,7 @@
 import BotClient from './Client';
 
-async function connectToWhatsApp() { // DENO point
-	const bot = new BotClient('auth_info_baileys');
-	// auth_file é uma pasta com autenticação para fazer login
-	// sem precisar escanear o QR Code novamente
+const bot = new BotClient('auth_info_baileys');
+// auth_info_baileys é uma pasta com autenticação para
+// fazer login sem precisar escanear o QR Code novamente
 
-	bot.connect();
-}
-
-connectToWhatsApp();
+bot.connect();
