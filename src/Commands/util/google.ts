c3670218ca08a2fb0548481c142bd086da105d41
Sunf3r <sunf3r@proton.me>
2023-10-25 00:24:00 +0000

    google cmd improvements

--- src/Commands/util/google.ts
+++ src/Commands/util/google.ts
@@ -1,7 +1,7 @@
 import { CmdContext } from '../../Core/Typings/types.js';
-import { isEmpty } from '../../Core/Components/Utils.js';
 import Command from '../../Core/Classes/Command.js';
-import google from 'googlethis';
+import * as g from 'google-sr';
+import googleThis from 'googlethis';
 
 export default class extends Command {
 	constructor() {
@@ -13,69 +13,81 @@ export default class extends Command {
 	async run({ args, bot, msg, user, sendUsage, t }: CmdContext) {
 		if (!args[0]) return sendUsage();
 
-		let img = '';
-		let source = '\n'; // data & img links
 		let text = ''; // it's what the bot will say
-		const query = await google.search(args.join(' ').trim(), {
-			page: 0,
-			safe: false, // Safe Search
-			parse_ads: false, // If set to true sponsored results will be parsed
-			additional_params: {
-				/* add additional parameters here,
-                see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters
-                and https://www.seoquake.com/blog/google-search-param/
-                */
-				hl: user.lang,
+		const query = await g.search({
+			query: args.join(' ').trim(),
+			safeMode: false,
+			filterResults: [
+				g.ResultTypes.DictionaryResult,
+				g.ResultTypes.TranslateResult,
+				g.ResultTypes.CurrencyResult,
+				g.ResultTypes.SearchResult,
+				g.ResultTypes.TimeResult,
+			],
+			requestConfig: {
+				params: {
+					hl: user.lang,
+					lr: `lang_${user.lang}`.toLowerCase(),
+				},
 			},
 		});
 
 		if (!query) return bot.send(msg, t('search:notFound'));
 
 		const responseFunctions = {
-			knowledge_panel() {
-				const { title, type, description, url, metadata, images } = query.knowledge_panel;
-				source += `URL: ${url}`;
+			SEARCH(r: g.SearchResultNode) {
+				const { title, link, description } = r;
 
-				text += `*${title}* (${type}) \n\n` + '> ' + (description || '');
+				text += `*${title}:*\n> ${description} (${link})`;
+			},
+			async DICTIONARY(r: g.DictionaryResultNode) {
+				const moreInfo = await googleThis.search(r.word, {
+					parse_ads: false,
+					safe: false,
+					additional_params: { hl: user.lang },
+				});
+
+				const panel = moreInfo?.knowledge_panel;
+
+				text += `*${panel?.title}* (${panel?.type})\n> ` + (panel?.description || '');
 				// e.g.: *Donald Trump* (45th U.S. President)
 				// > Donald John Trump is bla bla bla
 
-				for (const { title, value } of metadata) text += `\n*${title}*: ${value}\n`;
-				// e.g.: Age: 69
-
-				if (images[0]) {
-					img = images[0].url;
-
-					source += '\nImage source: ' + images[0].source || 'not found';
-				}
+				for (const { title, value } of panel?.metadata) text += `\n*${title}*: ${value}`;
 			},
-			videos() {
-				for (let i = 0; i < 3; i++) {
-					const { title, author, duration, url } = query.videos[i];
+			CURRENCY(r: g.CurrencyResultNode) {
+				const { formula } = r;
 
-					text += `video: (${title} - ${author})[${url}] [${duration}]`;
-				}
+				text += `*${formula}*`;
+
+				query.length = 1;
 			},
-			results() {
-				for (let i = 0; i < 3; i++) {
-					const { title, description, is_sponsored, url } = query.results[i];
+			TIME(r: g.TimeResultNode) {
+				const { timeInWords } = r;
 
-					if (is_sponsored) return;
+				text += `*${timeInWords}*`;
 
-					text += `\n[${title}](${url}): \n ${description || ''}\n`;
-					// Title hyperlink: description
-				}
+				query.length = 1;
+			},
+			TRANSLATE(r: g.TranslateResultNode) {
+				const { source: src, translation: res } = r;
+
+				text += `${src.language}  ➟  ${res.language}\n`;
+				text += `*${res.text}* (${res.pronunciation})`;
+
+				query.length = 1;
 			},
 		};
 
-		for (const [key, value] of Object.entries(query)) {
-			const objLiteral = responseFunctions[key as 'videos'];
+		query.length = 3; // limit to 3 results
 
-			if (!isEmpty(value) && objLiteral) objLiteral();
+		for (const result of query) {
+			text += '\n\n';
+
+			await responseFunctions[result.type as 'SEARCH'](result as any);
 		}
 
-		text += source;
-
-		return bot.send(msg, { text, image: { url: img } });
+		bot.send(msg, text.trim());
+		return;
 	}
 }
