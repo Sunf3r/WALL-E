c295176d6a502ddf105ad0ac3e0e45e1af3d24e5
gustar09 <gustavo.rodrigues37089@gmail.com>
2023-11-16 15:31:36 -0300

    clear final

--- src/Commands/util/clear.ts
+++ src/Commands/util/clear.ts
@@ -13,10 +13,10 @@ export default class extends Command {
     }
 
     async run ({ bot, msg, args, group }: CmdContext) {
-        console.log(args[0], Number.isNaN(args[0]))
-        if (!Number.isNaN(args[0]) || Number(args[0]) <= 0) return bot.send(msg.chat, "Oi amigo, você esqueceu de dizer quantas menssagens você quer limpar, ou informou da forma incorreta ☝️");
+        let qnt = Number(args[0]);
+        if (!Number.isNaN(qnt) || Number(qnt) <= 0) return bot.send(msg.chat, "Oi amigo, você esqueceu de dizer quantas menssagens você quer limpar, ou informou da forma incorreta ☝️");
+        qnt = qnt > this.limit ? this.limit : qnt;
 
-        const qnt = Number(args[0]) > this.limit ? this.limit : Number(args[0]);
         const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
 
         group?.getCachedMsgs(qnt).forEach(async groupMsg => {
