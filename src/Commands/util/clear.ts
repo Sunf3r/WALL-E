ab20bdb432440401a192458549e73acb23b4a52c
gustar09 <gustavo.rodrigues37089@gmail.com>
2023-11-16 15:28:56 -0300

    clear final

--- src/Commands/util/clear.ts
+++ src/Commands/util/clear.ts
@@ -13,6 +13,7 @@ export default class extends Command {
     }
 
     async run ({ bot, msg, args, group }: CmdContext) {
+        console.log(args[0], Number.isNaN(args[0]))
         if (!Number.isNaN(args[0]) || Number(args[0]) <= 0) return bot.send(msg.chat, "Oi amigo, você esqueceu de dizer quantas menssagens você quer limpar, ou informou da forma incorreta ☝️");
 
         const qnt = Number(args[0]) > this.limit ? this.limit : Number(args[0]);
