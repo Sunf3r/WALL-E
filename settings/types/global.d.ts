243876c49ab8740f2d3b03a1512772ce5cca5ad3
Sunf3r <sunf3r@proton.me>
2024-06-29 12:27:40 +0000

    add ts types

---
+++ settings/types/global.d.ts
@@ -0,0 +1,14 @@
+type str = string;
+type num = number;
+type bool = boolean;
+
+interface String {
+	encode(): str;
+	toPascalCase(): str;
+	align(limit: num, endPosition?: boolean): str;
+	filterForRegex(): str;
+}
+
+interface Number {
+	bytes(onlyNumbers?: bool): string | number;
+}
