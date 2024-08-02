e7d5829fc6986a18e388b63f850a07e1ca755cd1
Sunf3r <sunf3r@proton.me>
2024-08-02 21:48:07 +0000

    add subCmds support to Cmd Class

--- class/cmd.ts
+++ class/cmd.ts
@@ -3,6 +3,7 @@ import type { CmdCtx } from '../map.js'
 export default abstract class Cmd {
 	name: str
 	aliases: str[]
+	subCmds: str[]
 	cooldown: num
 	access: Partial<{
 		dm: bool
@@ -14,6 +15,7 @@ export default abstract class Cmd {
 		this.name = ''
 		this.aliases = c.aliases || []
 		this.cooldown = c.cooldown === 0 ? 0 : c.cooldown || 3 // Ignore some cmds cooldown
+		this.subCmds = []
 		this.access = Object.assign({
 			dm: true,
 			groups: true,
