2b613f1e60e98c9e1d216a0d022764d38a22c45f
Sunf3r <sunf3r@proton.me>
2024-08-04 04:11:25 +0000

    add reminder list & invalid timeout check

--- cmd/util/remind.ts
+++ cmd/util/remind.ts
@@ -4,16 +4,47 @@ export default class extends Cmd {
 	constructor() {
 		super({
 			alias: ['re', 'lembrete'],
+			subCmds: ['list'],
 		})
 	}
 
 	async run({ bot, user, msg, args, sendUsage }: CmdCtx) {
+		if (args[0] === this.subCmds[0]) {
+			const list = await prisma.reminders.findMany({ where: { author: user.id } })
+
+			const pendingReminders = list
+				.filter((i) => !i.isDone)
+				.sort((a, b) => Number(a.remindAt) - Number(b.remindAt))
+				.map((l, i) => {
+					let duration = (Number(l.remindAt) - Date.now()).duration()
+
+					return `${i + 1}. \`${l.msg}\` in *${duration}*`
+				})
+				.join('\n')
+
+			const doneReminders = list
+				.filter((i) => i.isDone)
+				.sort((a, b) => Number(a.remindAt) - Number(b.remindAt))
+				.map((l, i) => `${i + 1}. \`${l.msg}\``)
+				.join('\n')
+
+			let text = ''
+
+			if (pendingReminders[0]) text += `*Reminders list:*\n${pendingReminders}\n\n`
+			if (doneReminders[0]) text += `*Done reminders:*\n${doneReminders}`
+
+			bot.send(msg, text.trim())
+			return
+		}
 		if (!args[1]) return sendUsage()
 
 		const matchMs = args.join(' ').toMs() // convert string to ms
 		// '1s' => 1000ms
 
-		const time = Date.now() + matchMs[0] // remint at this moment
+		// sendUsage if time is invalid or less then 1m
+		if (!matchMs[0] || matchMs[0] < 59_999) return sendUsage()
+
+		const time = Date.now() + matchMs[0] // remind at this moment
 
 		args = args
 			.filter((a) => !matchMs[1].includes(a)) // remove time from remind text
@@ -22,9 +53,8 @@ export default class extends Cmd {
 			data: { // create remind
 				author: user.id,
 				chat: msg.chat,
-				msg: args.join(' ').trim(), // remove unnecessary spaces from str
+				msg: args.join(' ').trim().slice(0, 512), // remove unnecessary spaces from str
 				remindAt: String(time),
-				//isDone
 			},
 		})
 
