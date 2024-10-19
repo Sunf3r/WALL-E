56c0de87073f303fd869be6c89c702e5b76a2fc4
Sunf3r <lucas_gif@proton.me>
2024-10-19 02:42:25 +0000

    add translate cmd

---
+++ src/Commands/util/translate.ts
@@ -0,0 +1,27 @@
+import Command from '../../Core/Command';
+import { CmdContext } from '../../Typings';
+import translate from 'google-translate';
+
+export default class extends Command {
+	constructor() {
+		super({
+			aliases: ['t'],
+			cooldown: 5,
+		});
+	}
+
+	async run(ctx: CmdContext) {
+		if (!ctx.args[1]) return ctx.bot.react(ctx.msg, '❌');
+
+		const options = {
+			to: ctx.args.shift(),
+		};
+		const t = await translate(ctx.args.join(' '), options);
+
+		const msg = '🌐 *Google Translate*\n' +
+			`*${t.from.language.iso}  ➟  ${options.to}*\n` +
+			'```\n' + t.text + '```';
+
+		return await ctx.bot.send(ctx.msg, msg);
+	}
+}
