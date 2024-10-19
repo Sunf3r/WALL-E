03cd142b7f581b33cb5c7d4015cfe66c7279be60
Sunf3r <lucas_gif@proton.me>
2024-10-19 02:42:25 +0000

    update eval cmd msg

--- src/Commands/admin/eval.ts
+++ src/Commands/admin/eval.ts
@@ -8,36 +8,26 @@ export default class extends Command {
 	constructor() {
 		super({
 			aliases: ['e'],
-			// react: false,
 			access: { onlyDevs: true },
 		});
 	}
 
 	async run(ctx: CmdContext) {
 		const lang = (langs.includes(ctx.args[0] as 'py') ? ctx.args.shift() : 'eval') as Lang;
-
-		let output = '';
-
 		const startTime = Date.now();
-		const startRAM = getRAM(true) as number;
 
-		output = await runOtherLang({ lang, code: ctx.args.join(' '), ctx });
+		let output = await runOtherLang({ lang, code: ctx.args.join(' '), ctx });
 
-		// difference between initial RAM and final RAM
 		const duration = (Date.now() - startTime).toLocaleString('pt');
-		const endRAM = getRAM(true) as number;
 
-		let RAMRange: str | num = Number((endRAM - startRAM).toFixed(2));
-		RAMRange = RAMRange < 0 ? RAMRange : `+${RAMRange}`;
-
-		const text = `*[👨‍💻] - ${lang.toUpperCase()}*\n` +
-			`[📊]: ${duration}ms - ` + `${endRAM}MB (${RAMRange}MB)\n` +
+		const text = `*[👨‍💻] - ${lang.toUpperCase()}* ` +
+			`${duration}ms - ${getRAM()}` + '\n\n' +
 			output.trim()
 				.slice(0, 256);
 
 		clearTemp();
 
-		const reply = await ctx.bot.send(ctx.msg, text);
+		ctx.bot.send(ctx.msg, text);
 		return;
 	}
 }
