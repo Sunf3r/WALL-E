bd9e5c557f553ba46213d065997de95175bcebf7
Sunf3r <lucas_gif@proton.me>
2024-10-19 02:42:26 +0000

    add dynamic sticker quality

--- src/Commands/util/sticker.ts
+++ src/Commands/util/sticker.ts
@@ -16,40 +16,42 @@ export default class extends Cmd {
 	async run({ msg, bot, args, user, group, sendUsage, t }: CmdContext) {
 		if (!msg.isMedia && !msg.quoted) return sendUsage();
 
-		let stickerTypes = ['rounded', 'full', 'crop', 'circle'];
-		let targetMsg = msg.quoted && msg.quoted.isMedia ? msg.quoted : msg;
-
+		let targetMsg = msg.isMedia ? msg : msg.quoted;
 		let buffer = await bot.downloadMedia(targetMsg);
+
 		if (!buffer) return bot.send(msg, t('sticker.nobuffer'));
 
 		await bot.react(msg, '⌛');
-
-		if (args[0] === 'rmbg') {
-			const name = Math.random();
-
-			writeFileSync(`temp/${name}.webp`, buffer);
-			await runCode({
-				file: 'src/Core/Plugins/removeBg.py',
-				code: `temp/${name}.webp temp/${name}.png`,
-			});
-			buffer = readFileSync(`temp/${name}.png`) || buffer;
-
-			cleanTemp();
-		}
+		let stickerTypes = ['rounded', 'full', 'crop', 'circle'];
+		let quality = 15;
 
 		switch (targetMsg.type) {
 			case 'video':
 				stickerTypes = ['full', 'rounded'];
+				quality = 1;
 				break;
 			case 'sticker':
 				await bot.send(msg, { image: buffer, gifPlayback: true });
+			case 'image':
+				if (args[0] === 'rmbg') {
+					const name = Math.random();
+
+					writeFileSync(`temp/${name}.webp`, buffer);
+					await runCode({
+						file: 'src/Core/Plugins/removeBg.py',
+						code: `temp/${name}.webp temp/${name}.png`,
+					});
+					buffer = readFileSync(`temp/${name}.png`) || buffer;
+
+					cleanTemp();
+				}
 		}
 
 		for (const type of stickerTypes) {
 			const metadata = new Sticker(buffer!, {
 				...genStickerMeta(user, group),
 				type,
-				quality: 30,
+				quality,
 			});
 
 			await bot.send(msg.chat, await metadata.toMessage());
