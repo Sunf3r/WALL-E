abeeb4c8037e9bbf83a69bc85d4e37e781cf912a
Sunf3r <lucas_gif@proton.me>
2024-10-19 02:42:26 +0000

    clear final

--- src/Commands/util/clear.ts
+++ src/Commands/util/clear.ts
@@ -13,6 +13,7 @@ export default class extends Command {
     }
 
     async run ({ bot, msg, args, group }: CmdContext) {
+        console.log(args[0])
         if (Number.isNaN(args[0])) return bot.send(msg.chat, "Oi amigo, você esqueceu de dizer quantas menssagens você quer limpar ☝️");
 
         const qnt = Number(args[0]) > this.limit ? this.limit : Number(args[0]);
