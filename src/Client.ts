4ea4d56594fd8b3e4817ef0ba66a081e331ba33f
Sunf3r <lucas_gif@proton.me>
2024-10-19 02:42:25 +0000

    small aesthetic changes

--- src/Client.ts
+++ src/Client.ts
@@ -7,50 +7,56 @@ import makeWASocket, {
 import { readdirSync } from 'fs'; // DENO point
 
 export default class BotClient {
-	authFile: string;
+	auth: string;
 	sock!: ReturnType<typeof makeWASocket>;
 	cmds: Map<string, Command>;
 	events: Map<string, Function>;
 	aliases: Map<string, string>;
 
-	constructor(authFile: string) {
-		this.authFile = authFile; // arquivo de autenticação
+	constructor(auth: string) {
+		this.auth = auth; // pasta de autenticação
 		this.sock;
-		this.events = new Map<string, Function>();
-		this.cmds = new Map<string, Command>();
-		this.aliases = new Map<string, string>();
+		this.events = new Map<string, Function>(); // Map de eventos
+		this.cmds = new Map<string, Command>(); // Map de comandos
+		this.aliases = new Map<string, string>(); // Map de aliases de comandos
 	}
 
 	async connect() {
-		const { state, saveCreds } = await useMultiFileAuthState(this.authFile);
+		const { state, saveCreds } = await useMultiFileAuthState(this.auth);
 		// recupera a sessão
 		this.sock = makeWASocket({ auth: state, printQRInTerminal: true });
 		// abre o socket
 		this.sock.ev.on('creds.update', saveCreds);
 		// atualiza as credenciais de login
 
-		await this.fileHandler(`${__dirname}/Commands`, this.loadCommands);
-		// Carrega os comandos
-		this.fileHandler(`${__dirname}/Events`, this.loadEvents);
-		// Carrega os eventos
+		// Carregando comandos
+		await this.folderHandler(`${__dirname}/Commands`, this.loadCommands);
+		// folderHandler() vai ler uma pasta e chamar a função fornecida
+		// para cada arquivo contido nela
+		this.folderHandler(`${__dirname}/Events`, this.loadEvents);
+		// Carregando eventos
 	}
 
 	async send(chatId: string, content: string | AnyMessageContent, reply?: proto.IWebMessageInfo) {
+		// Função intermediária para facilitar o envio de msgs
 		return await this.sock.sendMessage(
-			chatId!,
+			chatId!, // ID do chat
 			typeof content === 'string' ? { text: content } : content,
+			// corpo da solicitação
 			reply ? { quoted: reply! } : {},
+			// Msg a ser respondida
 		);
 	}
 
-	async fileHandler(path: string, handler: Function) {
+	async folderHandler(path: string, handler: Function) {
 		for (const category of readdirSync(path)) {
 			// Para cada pasta de categorias
 			for (const file of readdirSync(`${path}/${category}`)) {
 				// Para cada arquivo de cada categoria
 				const imported = await import(`file:${path}/${category}/${file}`);
 
-				handler.bind(this)(file, category, imported);
+				// chama a função fornecida para este arquivo
+				await handler.bind(this)(file, category, imported);
 			}
 		}
 	}
@@ -64,7 +70,7 @@ export default class BotClient {
 			cooldown: 3,
 		} as Command, cmd);
 		// Isso vai comparar as propriedades do cmd com esse "template"
-		// Os dados que o cmd não tiver, serão preenchidas
+		// Os dados que o cmd não tiver, serão preenchidas com o default
 
 		properties.access = Object.assign({
 			dm: false,
@@ -73,21 +79,27 @@ export default class BotClient {
 		}, properties.access);
 		// Compara as permissões do comando
 
+		// Seta o cmd dentro do Map
 		this.cmds.set(properties.name!, properties);
 
 		properties.aliases
 			?.forEach((a) => this.aliases.set(a, properties.name!));
-		// Salva o comando no Map
+		// Seta as aliases do cmd
 	}
 
 	async loadEvents(file: string, category: string, imported: any) {
 		const event = imported.default.default;
 		const name = `${category}.${file.slice(0, -3)}`;
-		// os nomes dos arquivos e pastas são exatamente os nomes dos eventos
+		// as pastas e arquivos seguem a mesma nomenclatura
+		// que a lib usa em seus eventos
 		this.events.set(name, event);
 
+		// O evento já é declarado aqui
 		this.sock.ev.on(name as keyof BaileysEventMap, (...args) => {
+			// Mas quando for chamado, vai consultar no Map
+			// Isso permite modificar eventos em runtime
 			this.events.get(name)!(this, ...args);
+			// eventFunction(this, ...args);
 		});
 	}
 }
