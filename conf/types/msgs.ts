f1d17475dde6b780384a88e00f268bb91b2ec246
EduLGFon <edulgfon@gmail.com>
2026-09-07 13:41:13 -0300

    chore(deadcode): trim unused msgs type exports

--- conf/types/msgs.ts
+++ conf/types/msgs.ts
@@ -59,4 +59,4 @@ const mediaValues = Object.values(mediaTypes)
 const isMedia = (type: MsgTypes) => mediaValues.includes(type)
 const isVisual = (type: MsgTypes) => visualValues.includes(type)
 
-export { allMsgTypes, coolTypes, coolValues, isMedia, isVisual, mediaTypes, textTypes, visualTypes }
+export { allMsgTypes, coolValues, isMedia, isVisual }
